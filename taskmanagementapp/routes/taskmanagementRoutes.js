68697cbd510e8e4b865f601e84ed96698c619efc
Wendy Alfaro <166208157+WenYanelly16@users.noreply.github.com>
2025-03-04 18:33:34 +0000

    Added add-task-route

--- taskmanagementapp/routes/taskmanagementRoutes.js
+++ taskmanagementapp/routes/taskmanagementRoutes.js
@@ -1,1 +1,23 @@
 //Filename: ./routes/taskmanagementRoute.js*/
+
+import express from "express";
+import path from "path";
+const router = express.Router();
+
+let tasks = [];
+let idCounter = 1;
+
+// Display the main page with the list of tasks
+router.get('/', (req, res) => {
+    res.render('tasks', {tasks});
+});
+
+// Handle form submissions to add a new task
+router.post('/add-task', (req, res) => {
+    const { title, description } = req.body;
+    tasks.push({ id: idCounter++, title, description, completed: false });
+    res.redirect('/');
+});
+
+
+    export default router;
